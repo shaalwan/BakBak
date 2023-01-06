a6b4e3e3dbc174ed278e1c46cc6c573bde266975
shubham salwan <ssalwan20@gmail.com>
2023-01-06 22:04:18 +0530

    pkg

--- backend/main.go
+++ backend/main.go
@@ -2,54 +2,25 @@ package main
 
 import (
     "fmt"
-    "log"
     "net/http"
-
-    "github.com/gorilla/websocket"
+    "github.com/shaalwan/BakBak/pkg/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-    ReadBufferSize:  1024,
-  WriteBufferSize: 1024,
-  CheckOrigin: func(r *http.Request) bool { return true },
-}
-func reader(conn *websocket.Conn) {
-    for {
-        messageType, p, err := conn.ReadMessage()
-        if err != nil {
-            log.Println(err)
-            return
-        }
-        fmt.Println(string(p))
-
-        if err := conn.WriteMessage(messageType, p); err != nil {
-            log.Println(err)
-            return
-        }
-
-    }
-}
-
 func serveWs(w http.ResponseWriter, r *http.Request) {
-    fmt.Println(r.Host)
-    ws, err := upgrader.Upgrade(w, r, nil)
+    ws, err := websocket.Upgrade(w, r)
     if err != nil {
-        log.Println(err)
-  }
-
-    reader(ws)
+        fmt.Fprintf(w, "%+V\n", err)
+    }
+    go websocket.Writer(ws)
+    websocket.Reader(ws)
 }
 
 func setupRoutes() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Simple Server")
-  })
-
     http.HandleFunc("/ws", serveWs)
 }
 
 func main() {
-    fmt.Println("Chat App v0.01")
+    fmt.Println("Distributed Chat App v0.01")
     setupRoutes()
     http.ListenAndServe(":8080", nil)
 }
